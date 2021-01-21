da8ba6696d2540b325092154e8ace122a4e4eed2
MarinPuizina <marinpuizina@hotmail.com>
2021-01-21 19:54:50 +0100

    Add Select query

--- main.go
+++ main.go
@@ -8,8 +8,12 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
+type Client struct {
+	Name string
+}
+
 func main() {
-	fmt.Println("Go MySQL")
+	fmt.Println("-----Trying to connect to the MySQL databse")
 
 	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/bank")
 
@@ -18,5 +22,25 @@ func main() {
 	}
 	defer db.Close()
 
-	fmt.Println("Successfully connected to the MySQL databse")
+	fmt.Println("-----Successfully connected to the MySQL databse")
+
+	getRows(db)
+}
+
+func getRows(db *sql.DB) {
+	results, err := db.Query("SELECT name FROM bank.clients")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for results.Next() {
+		var client Client
+
+		err = results.Scan(&client.Name)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println("Client name=" + client.Name)
+	}
 }

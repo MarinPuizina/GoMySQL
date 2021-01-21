abe5d4cb2e52ca5d5bf53f99e07a9f1e7eb79b2d
MarinPuizina <marinpuizina@hotmail.com>
2021-01-21 17:06:15 +0100

    Add go.mod and Open() db connection

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
 	fmt.Println("Go MySQL")
+
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/bank")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	fmt.Println("Successfully connected to the MySQL databse")
 }
